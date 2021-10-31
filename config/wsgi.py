4e3627f6893a44c6050c78f84c17906b236b05ad
alevsandra <abrzeszczynska@gmail.com>
2021-10-31 11:44:44 +0100

    dj-static config

--- config/wsgi.py
+++ config/wsgi.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 from pictionary_db.models import Category, TempCategory
 
 # startup code
@@ -20,4 +21,4 @@ while len(TempCategory.objects.all()) < 5:
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
