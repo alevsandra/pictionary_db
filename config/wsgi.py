cc8e4fc052e7c28d4df127c7f0d924dabc2f0877
alevsandra <abrzeszczynska@gmail.com>
2021-11-06 22:51:49 +0100

    Models dynamic import

--- config/wsgi.py
+++ config/wsgi.py
@@ -11,7 +11,10 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
-from pictionary_db.models import Category, TempCategory
+from django.apps import apps
+
+Category = apps.get_model('pictionary_db', 'Category')
+TempCategory = apps.get_model('pictionary_db', 'TempCategory')
 
 # startup code
 while len(TempCategory.objects.all()) < 5:
